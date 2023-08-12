dc9da521add0642cb52bf7ee5968dda5acc6ee96
shamspias <shamsuddinpias0@gmail.com>
2023-08-13 04:00:47 +0600

    make demo of llm

--- models/llm_helpers.py
+++ models/llm_helpers.py
@@ -1,8 +1,30 @@
+import openai
+
+
 class LanguageModelRequest:
+    """ 
+    A class to handle requests to a language model (e.g., GPT-3).
+    
+    This class provides methods to send questions to a language model and retrieve responses.
+    """
+
     def __init__(self):
-        self.data = ["users", "orders"]
+        # Assuming you've set OPENAI_API_KEY in your .env file
+        # This line should be uncommented and set up in a real-world scenario
+        # openai.api_key = os.getenv('OPENAI_API_KEY')
+        pass
 
-    def process(self, question, data):
-        response_text = ""
+    def ask_llm(self, question, data):
+        """ 
+        Send a question with data to GPT-3 and get the response.
+        
+        For this mock implementation, we're just returning a simulated response. 
+        In a real-world scenario, this method should make an actual request to GPT-3.
+        """
+        # Simulated behavior for demonstration
+        return f"Response from GPT-3 for question: {question} with data: {data}"
 
-        return response_text
+        # Real-world request to GPT-3 (requires OpenAI API key and setup)
+        # Uncomment and use this in a real-world scenario
+        # response = openai.Completion.create(engine="davinci", prompt=f"{question} {data}", max_tokens=150)
+        # return response.choices[0].text.strip()
