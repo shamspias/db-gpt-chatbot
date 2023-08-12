46a89a1390c0d792f20c70bcf52675af8722a144
shamspias <shamsuddinpias0@gmail.com>
2023-08-13 03:44:02 +0600

    feat: llms works

---
+++ models/llm_helpers.py
@@ -0,0 +1,8 @@
+class LanguageModelRequest:
+    def __init__(self):
+        self.data = ["users", "orders"]
+
+    def process(self, question, data):
+        response_text = ""
+
+        return response_text
